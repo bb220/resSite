6051acc797d6a92896aec070d86aa9c404e9d620
Brandon <bellero.brandon@gmail.com>
2015-03-09 19:47:22 -0400

    jquery added to event descriptions

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@
         var $red = $('#red');
         var $bottomRed = $('#bottomRed');
         var $menuToggle = $('#menuToggle');
+        
+
 
       //Sets the height of the landing page to fit the screen, 80px is the height of the navbar
         $topPage.css("height", viewportHeight - 70);
@@ -126,7 +128,7 @@
      
 
       //slogan fade
-        $slogan.delay(900).fadeTo(200, 1);
+        $slogan.delay(900).fadeTo(100, 1);
         
       //On click, navbar links scroll to corresponding section
         $aboutBtn.click(function(){
@@ -170,4 +172,74 @@
 
 
       });
+
+    //DOM Cache
+    var $eventOne = $('#eventOne');
+    var $eventTwo = $('#eventTwo');
+    var $eventThree = $('#eventThree');
+    var $eventFour = $('#eventFour');
+    var $eventFive = $('#eventFive');
+    var $eventSix = $('#eventSix');
+    var $eventEight = $('#eventEight');
+    var $eventSeven = $('#eventSeven');
+    var $descOne = $('#descOne');
+    var $descTwo = $('#descTwo');
+    var $descThree = $('#descThree');
+    var $descFour = $('#descFour');
+    var $descFive = $('#descFive');
+    var $descSix = $('#descSix');
+    var $descSeven = $('#descSeven');
+    var $descEight = $('#descEight');
+  
+
+
+    //On click, Event Description appears
+   
+    $eventOne.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descOne.addClass('active');
+    });
+
+    $eventTwo.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descTwo.addClass('active');
+    });
+
+    $eventThree.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descThree.addClass('active');
+    });
+
+    $eventFour.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descFour.addClass('active');
+    });
+
+    $eventFive.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descFive.addClass('active');
+    });
+
+    $eventSix.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descSix.addClass('active');
+    });
+
+    $eventSeven.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descSeven.addClass('active');
+    });
+
+    $eventEight.click(function(){
+      var $active = $('#descriptions').find('.active');
+      $active.removeClass('active');
+      $descEight.addClass('active');
+    });
     
